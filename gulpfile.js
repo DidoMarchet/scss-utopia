449ba3dee8cdc28790502ef57a52c17429228a50
Davide Marchet <38940340+DidoMarchet@users.noreply.github.com>
2021-10-26 10:46:17 +0200

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,9 @@ const plugins = require('gulp-load-plugins')();
 exports.default = function() {
   return gulp.src(
     [
-      'node_modules/sass-unitconverter/_unitconverter.scss',
-      'src/queries.scss',
-      'src/utils.scss',
+      'src/unitconverter.scss',
+      'src/options.scss',
       'src/react.scss',
-      'src/resp.scss',
-      'src/fluid.scss',
       'src/slope.scss'
     ])
     .pipe(plugins.concat('index.scss'))
